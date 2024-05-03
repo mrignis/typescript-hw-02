168a57c30d8524462d80a290f4cf46a6b6e1b743
mrignis <chelbarah9678@gmail.com>
2024-05-03 14:05:03 -0400

    Update ImageGallery.tsx

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -11,16 +11,11 @@ type ImageGalleryProps = {
 const ImageGallery: React.FC<ImageGalleryProps> = ({ images, onImageClick }) => {
   // Перевірка наявності images перед використанням
   if (!images || images.length === 0) {
-    return <div>No images to display</div>;
-  }
-
-  return (
-    <ul className={styles.gallery}>
-      {images.map((image) => (
-        <ImageCard key={image.id} image={image} onImageClick={onImageClick} />
-      ))}
-    </ul>
-  );
-};
+    return  <ul className={styles.gallery}>
+    {images.map((image) => (
+      <ImageCard key={image.id} image={image} onImageClick={onImageClick} />
+    ))}
+  </ul>
+);;
 
 export default ImageGallery;
