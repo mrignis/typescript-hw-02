fa3a05a01a48df999a5befc5aa2151f85848bfae
mrignis <chelbarah9678@gmail.com>
2024-05-03 13:43:38 -0400

    Update ImageCard.tsx

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -4,7 +4,7 @@ import styles from "./ImageCard.module.css";
 import { Image } from "../image";
 
 type ImageCardProps = {
-  image: Image[];
+  image: Image;
   onImageClick: (image: Image) => void;
 };
 
