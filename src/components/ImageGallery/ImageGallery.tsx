de9e450ef5e00c751c3dc7bcbc69c81e471d569e
mrignis <chelbarah9678@gmail.com>
2024-05-03 14:06:48 -0400

    гз

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ImageCard from "../ImageCard/ImageCard";
 import styles from "./ImageGallery.module.css";
-import { Image } from "../temple";
+import { Image } from "../types";
 
 type ImageGalleryProps = {
   images: Image[];
@@ -9,14 +9,13 @@ type ImageGalleryProps = {
 };
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({ images, onImageClick }) => {
-  // Перевірка наявності images перед використанням
-  if (!images || images.length === 0) {
-    return  <ul className={styles.gallery}>
-    {images.map((image) => (
-      <ImageCard key={image.id} image={image} onImageClick={onImageClick} />
-    ))}
-  </ul>
-  } 
+  return (
+    <ul className={styles.gallery}>
+      {images && images.length > 0 && images.map((image) => (
+        <ImageCard key={image.id} image={image} onImageClick={onImageClick} />
+      ))}
+    </ul>
+  );
 };
 
 export default ImageGallery;
